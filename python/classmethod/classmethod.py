7f5537b687d5be145570e1f2087c7cf437bbc983
David G. Quintas <dgquintas@gmail.com>
2012-04-25 01:16:33 -0700

    inheritance of classmethods

--- python/classmethod/classmethod.py
+++ python/classmethod/classmethod.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 class C(object):
 
   @classmethod
@@ -6,9 +7,23 @@ class C(object):
 
   @classmethod
   def printFoo(cls):
-    print cls.foo
+    print("cls.foo")
 
+class D(C):
+    
+    @classmethod
+    def printFoo(cls):
+        print("HOLAAAAAAA")
+        import platform
+        if platform.python_version_tuple()[0] == 3:
+            super().printFoo() #python3
+        else:
+            super(D, cls).printFoo() #python2
+        print("ADIOSSSSS")
 
 c = C()
 c.printFoo()
 C.printFoo()
+print()
+d = D()
+d.printFoo()
